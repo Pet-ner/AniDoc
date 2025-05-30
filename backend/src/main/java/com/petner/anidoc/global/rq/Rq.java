0dc69df3ba510ccb5ed125d59f291c3b448f9d39
vincentius <parkok6143@gmail.com>
2025-05-30 18:33:17 +0900

    security 추가

--- backend/src/main/java/com/petner/anidoc/global/rq/Rq.java
+++ backend/src/main/java/com/petner/anidoc/global/rq/Rq.java
@@ -80,7 +80,6 @@ public class Rq {
     public void setCookie(String name, String value){
         ResponseCookie cookie = ResponseCookie.from(name, value)
                 .path("/")
-                .domain("localhost")
                 .sameSite("Strict")
                 .secure(true)
                 .httpOnly(true)
