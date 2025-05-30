d155a4c12eb875863f348077d17bf47f5443e33e
vincentius <parkok6143@gmail.com>
2025-05-30 17:44:26 +0900

    corsconfig 테스트용으로 변경

--- frontend/src/components/HospitalCombobox.tsx
+++ frontend/src/components/HospitalCombobox.tsx
@@ -41,8 +41,7 @@ export default function HospitalCombobox({
       setLoading(true);
       try {
         const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/vets`,
-          { credentials: "include" }
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/vets`
         );
 
         if (!response.ok) {
