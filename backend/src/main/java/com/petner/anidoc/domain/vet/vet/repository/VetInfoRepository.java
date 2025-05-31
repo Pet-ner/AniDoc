cc7575c06560e35e0436a5c32fdbc0c4520cfe9e
Jaehyun Yoo <mordor8378@gmail.com>
2025-05-31 14:47:55 +0900

    Feat: VetInfoInitializer 추가

--- backend/src/main/java/com/petner/anidoc/domain/vet/vet/repository/VetInfoRepository.java
+++ backend/src/main/java/com/petner/anidoc/domain/vet/vet/repository/VetInfoRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VetInfoRepository extends JpaRepository<VetInfo, Long> {
+    boolean existsByVetNumber(String vetNumber);
 }
