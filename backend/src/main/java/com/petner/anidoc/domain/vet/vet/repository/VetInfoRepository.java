e24168a49eb5758512e43fe8a94f373c9aa9b98a
Youngheun <heeun.deco@gmail.com>
2025-06-02 05:26:14 +0900

    feat : User Initializer

--- backend/src/main/java/com/petner/anidoc/domain/vet/vet/repository/VetInfoRepository.java
+++ backend/src/main/java/com/petner/anidoc/domain/vet/vet/repository/VetInfoRepository.java
@@ -4,7 +4,10 @@ import com.petner.anidoc.domain.vet.vet.entity.VetInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VetInfoRepository extends JpaRepository<VetInfo, Long> {
     boolean existsByVetNumber(String vetNumber);
+    Optional<VetInfo> findByVetNumber(String vetNumber);
 }
